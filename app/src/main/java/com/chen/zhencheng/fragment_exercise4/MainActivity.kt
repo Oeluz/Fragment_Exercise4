e7d94d05db363e60a8c468781aa6db466a329afe
Zhencheng Chen <42793066+Oeluz@users.noreply.github.com>
2021-10-19 20:07:00 -0400

    Initial commit

---
+++ app/src/main/java/com/chen/zhencheng/fragment_exercise4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chen.zhencheng.fragment_exercise4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
